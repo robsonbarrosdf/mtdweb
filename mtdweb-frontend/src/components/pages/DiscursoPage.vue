e519f308777b86912368d13f0e95850e31edf4a4
Robson Barros <robsonbarrosdf@gmail.com>
2019-03-09 00:25:00 -0300

    Uso de eventos na página de discursos

--- mtdweb-frontend/src/components/pages/DiscursoPage.vue
+++ mtdweb-frontend/src/components/pages/DiscursoPage.vue
@@ -1,18 +1,22 @@
 <template>
     <div id='discursoPage' class='conteudo2'>
 
-        <Menu v-show='isMenuVisible' :showSearchParams='false' />
+        <Menu v-show='isMenuVisible' 
+            :showSearchParams='false' 
+            :discursosDoUsuario='discursosDoUsuario'
+            @click-discurso='clickDiscurso' />
 
         <div class='conteudoLateral'>
             <MenuButton />
             <PageTitle />
-            <DiscursoForm />
+            <DiscursoForm ref='discursoForm'/>
         </div> 
         
     </div> 
 </template>
 
 <script>
+import axios from 'axios'
 import { mapState } from 'vuex'
 import Menu from '@/components/template/Menu'
 import MenuButton from '@/components/pages/MenuButton'
@@ -21,12 +25,59 @@ import DiscursoForm from './DiscursoForm'
 export default {
     name: 'DiscursoPage',
     components: { Menu, MenuButton, PageTitle, DiscursoForm },
+    data: function() {
+        return {
+            discursoCorrente: {},
+            discursosDoUsuario: []
+        }
+    },
     computed: mapState(['isMenuVisible']),
+    methods: {
+        loadDiscursosDoUsuario() {
+            
+            axios.get("http://localhost:4000/discursos/usuario/6406")
+            .then(res => {
+
+                this.discursosDoUsuario = res.data;
+
+                this.discursosDoUsuario = this.discursosDoUsuario.map(d => {
+                    return {
+                        ...d,
+                        tituloDiscurso: d.txtTipoEncontro + ' ' + d.numEncontro + (d.txtTipoEncontro=='Sessão' ? '.' + d.anoEncontro : '') + ' - Discurso ' + d.nuOrdem                       
+                    }
+                })          
+
+            })
+
+        },        
+
+        // async clickDiscurso(discurso) {
+        //     let encontro = await this.getEncontro(346)
+        //     this.discursoCorrente = discurso
+        //     this.$refs.discursoForm.updateDiscursoSelecionado(discurso, encontro)
+        // },
+
+        clickDiscurso(discurso) {
+            this.getEncontro(346)
+                .then(encontro => {
+                    this.discursoCorrente = discurso
+                    this.$refs.discursoForm.updateDiscursoSelecionado(discurso, encontro)
+                })
+        },
+
+        getEncontro(codEncontro) {
+            return axios.get(`http://localhost:4000/encontros/${codEncontro}`)
+                .then(res => res.data)
+        }        
+    },
     beforeCreate() {
         // this.$store.commit("setPerfilSelecionado", 'Discurso');
         const rota = this.$route.path
         this.$store.commit("setPerfilSelecionado", rota);
     },
+    created() {
+        this.loadDiscursosDoUsuario()
+    }
 
 }
 </script>
--- mtdweb-frontend/src/components/pages/ListaDiscursos.vue
+++ mtdweb-frontend/src/components/pages/ListaDiscursos.vue
@@ -1,95 +1,52 @@
 <template>
-  <div id="listaDiscursos">
-    <b-table small borderless :items="meusDiscursos" :fields="colunas" @row-clicked="onRowClicked"></b-table>
-  </div>
+    <div id="listaDiscursos">
+        <b-table small borderless :items="discursosDoUsuario" :fields="colunas" @row-clicked="onRowClicked"></b-table>
+    </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
-  name: "ListaDiscursos",
-  data: function() {
-    return {
-      discursoComponentPage: {},
-      itemSelecionado: {},
-      meusDiscursos: [],
-    //   parametros: {
-    //     reuniao: true,
-    //     sessao: true,
-    //     dataInicial: new Date(),
-    //     dataFinal: new Date(),
-    //     titulo: null
-    //   },
-    //   page: 1,
-    //   count: 0,
-      colunas: [
-        { key: "tituloDiscurso", label: "Discursos", sortable: true, thClass: "cabecalho", tdClass: "dados"}
-      ]
-    };
-  },
-  methods: {
-
-    loadDiscursos() {
-        
-        axios.get("http://localhost:4000/discursos/usuario/6406")
-        .then(res => {
-
-            this.meusDiscursos = res.data;
-
-            this.meusDiscursos = this.meusDiscursos.map(d => {
-                return {
-                    ...d,
-                    tituloDiscurso: d.txtTipoEncontro + ' ' + d.numEncontro + (d.txtTipoEncontro=='Sessão' ? '.' + d.anoEncontro : '') + ' - Discurso ' + d.nuOrdem                       
+    name: "ListaDiscursos",
+    props: ['discursosDoUsuario'],
+    data: function() {
+        return {
+            discursoSelecionado: {},
+            colunas: [
+                {
+                    key: "tituloDiscurso",
+                    label: "Discursos",
+                    sortable: true,
+                    thClass: "cabecalho",
+                    tdClass: "dados"
                 }
-            })          
-
-            this.count = this.meusDiscursos.length;
-
-        })
-
+            ]
+        }
     },
-
-    // pesquisar(parametros) {
-    //   this.page = 1;
-    //   this.parametros = { ...parametros };
-    //   // console.log(this.parametros)
-    //   this.loadDiscursos();
-    // },
-
-    onRowClicked(item) { //onRowClicked(item, index, event) {
-      this.itemSelecionado = item;
-      this.$store.state.displayResultComponent.displayItemSelecionado(item);
+    methods: {
+        onRowClicked(item) {
+            //onRowClicked(item, index, event) {
+            this.discursoSelecionado = item;
+            this.$emit("click-discurso", item);
+        }
     }
-  },
-  created() {
-
-  },
-  mounted() {
-    this.loadDiscursos();
-  }
-};
+}
 </script>
 
 <style>
     #listaDiscursos {
-      height: 800px;;
-      display: block;
-      overflow: auto;
-      padding-top: 10px;
+        height: 800px;
+        display: block;
+        overflow: auto;
+        padding-top: 10px;
     }
 
     .cabecalho,
     .dados {
-      color: #fff;
+        color: #fff;
     }
 
     .cabecalho:hover,
     .dados:hover {
-      background-color: rgb(56, 170, 185);
+        background-color: rgb(56, 170, 185);
     }
-
-    #listaEncontros hr {
-        background-color: red;
-    }
-
 </style>
